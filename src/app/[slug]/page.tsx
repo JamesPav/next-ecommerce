ade8ef615d8c3998acbefb60e30ca8ed4120529b
James <mitspav@gmail.com>
2024-05-28 04:34:03 +0300

    single product page with customize buttons

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,67 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
+import ProductImages from "@/components/ProductImages";
 import React from "react";
 
 const SinglePage = () => {
-  return <div className=""></div>;
+  return (
+    <div className="px-4 py-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* IMG */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      {/* TEXTS */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi
+          molestiae voluptates eaque deserunt non pariatur, amet numquam
+          reiciendis at officiis, est perspiciatis alias. Veritatis neque autem
+          magnam error illo.
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">€69</h3>
+          <h2 className="font-medium text-2xl">€49</h2>
+        </div>
+
+        <div className="h-[2px] bg-gray-100" />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">PRODUCT INFO</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit cupiditate amet, necessitatibus fugiat laboriosam
+            distinctio molestiae aperiam deleniti! Officiis vero soluta aperiam
+            similique consequatur excepturi optio! Inventore placeat quod
+            necessitatibus?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">SHIPPING INFO</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit cupiditate amet, necessitatibus fugiat laboriosam
+            distinctio molestiae aperiam deleniti! Officiis vero soluta aperiam
+            similique consequatur excepturi optio! Inventore placeat quod
+            necessitatibus?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">RETURN & REFUND POLICY</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit cupiditate amet, necessitatibus fugiat laboriosam
+            distinctio molestiae aperiam deleniti! Officiis vero soluta aperiam
+            similique consequatur excepturi optio! Inventore placeat quod
+            necessitatibus?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://www.topcloset.gr/wp-content/uploads/2024/03/2100K1.jpg",
+  },
+  {
+    id: 2,
+    url: "https://www.topcloset.gr/wp-content/uploads/2024/03/2100K2.jpg",
+  },
+  {
+    id: 3,
+    url: "https://www.topcloset.gr/wp-content/uploads/2024/02/5501B.look2_.jpg",
+  },
+  {
+    id: 4,
+    url: "https://www.topcloset.gr/wp-content/uploads/2023/08/2010Br-595x893.jpg",
+  },
+];
 
 const ProductImages = () => {
-  return <div>ProductImages</div>;
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="h-[600px] w-[400px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          fill
+          className="object-cover rounded-md"
+          sizes="50vw"
+        />
+      </div>
+      <div className="flex justify-between gap-2  w-[400px] ">
+        {images.map((img, i) => (
+          <div
+            className="w-[80px] h-24 relative gap-2 mt-8 cursor-pointer"
+            key={img.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={img.url}
+              alt=""
+              fill
+              className="object-cover rounded-md"
+              sizes="50vw"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductImages;
